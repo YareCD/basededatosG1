3d1a90049d8601a3bc18fd9e01279c6422e3aea0
YareCD <figaromisi17@gmail.com>
2026-07-29 15:00:05 -0600

    Conceptos basicos de LQD

---
+++ 04-LenguajeEstructuradoDeConsultas/04-practices.sql
@@ -0,0 +1,19 @@
+/* ======================================================================
+DQL (Data Query Languaje) en SQLSERVER
+
+Archivo: 04-practices.sql
+
+Descripcion: En este archivo se trabaja con todo lo relacionado con la
+intruccion select
+========================================================================*/
+
+[dbo].[categorias]
+[dbo].[ciudades]
+[dbo].[clientes]
+[dbo].[departamentos]
+[dbo].[detalle_ventas]
+[dbo].[empleados]
+[dbo].[estados]
+[dbo].[productos]
+[dbo].[proveedores]
+[dbo].[ventas]
